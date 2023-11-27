f446096972b210b721b9c06f9417530186ac3468
Jim5e <jamesterenceclarit@gmail.com>
2023-11-27 22:44:47 +0800

    added to gh

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route , Link} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Homepage from './pages/Homepage.jsx'
 import Register from './pages/Register.jsx'
+import Appointments from './pages/Appointments.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path = "/" element = {<Homepage/>} />
         <Route path = "/Homepage" element = {<Homepage/>} />
         <Route path = "/Register" element = {<Register/>} />
+        <Route path = "/Appointments" element = {<Appointments/>} />
       </Routes>
     
     </BrowserRouter>
