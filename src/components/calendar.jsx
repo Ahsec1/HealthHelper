317a616d336c8130430abb44ab305218846e6cf3
Jim5e <134153707+Jim5e@users.noreply.github.com>
2023-11-28 19:31:17 +0800

    removed usless math()

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,18 +1,117 @@
-import React from 'react'
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { DateCalendar } from '@mui/x-date-pickers';
-import { DatePicker } from '@mui/x-date-pickers';
-import { Badge } from '@mui/material';
+import * as React from 'react';
+import dayjs from 'dayjs';
+import Badge from '@mui/material/Badge';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 
-function calendar() {
+//GET YOUR APPOINTMENTS FROM DATABASE
+function fakeFetch(date, { signal }) {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      const daysInMonth = date.daysInMonth();
+      const daysToHighlight = [1, 2, 3];
+
+      resolve({ daysToHighlight });
+    }, 500);
+
+    signal.onabort = () => {
+      clearTimeout(timeout);
+      reject(new DOMException('aborted', 'AbortError'));
+    };
+  });
+}
+
+const initialValue = dayjs(); //TODAYS DATE
+
+function ServerDay(props) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
+
   return (
-    <div className="flex items-center justify-center h-screen scale-120">
-      <LocalizationProvider dateAdapter={AdapterDateFns} >
-        <DateCalendar  showDaysOutsideCurrentMonth></DateCalendar>
-      </LocalizationProvider>
-    </div>
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? '🌚' : undefined}
+    >
+      <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
+    </Badge>
   );
 }
 
-export default calendar
+export default function DateCalendarServerRequest() {
+  const [selectedDate, setSelectedDate] = React.useState(null);
+  const requestAbortController = React.useRef(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+
+  const fetchHighlightedDays = (date) => {
+    const controller = new AbortController();
+    fakeFetch(date, {
+      signal: controller.signal,
+    })
+      .then(({ daysToHighlight }) => {
+        setHighlightedDays(daysToHighlight);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // ignore the error if it's caused by `controller.abort`
+        if (error.name !== 'AbortError') {
+          throw error;
+        }
+      });
+
+    requestAbortController.current = controller;
+  };
+
+  React.useEffect(() => {
+    fetchHighlightedDays(initialValue);
+    // abort request on unmount
+    return () => requestAbortController.current?.abort();
+  }, []);
+
+  const handleMonthChange = (date) => {
+    if (requestAbortController.current) {
+      // make sure that you are aborting useless requests
+      // because it is possible to switch between months pretty quickly
+      requestAbortController.current.abort();
+    }
+
+    setIsLoading(true);
+    setHighlightedDays([]);
+    fetchHighlightedDays(date);
+  };
+
+  const handleDateChange = (date) => {
+    let dateSelected = date.format('MM-DD-YYYY');
+    setSelectedDate(date);
+    console.log('Selected date:', dateSelected);
+   };
+
+  return (
+    <div className='flex items-center justify-center h-screen scale-120'>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        value = {initialValue}
+        loading={isLoading}
+        onMonthChange={handleMonthChange}
+        showDaysOutsideCurrentMonth
+        renderLoading={() => <DayCalendarSkeleton />}
+        onChange={handleDateChange}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          },
+        }}
+      />
+    </LocalizationProvider>
+    </div>
+  );
+}
